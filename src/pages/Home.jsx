a3178608e647cc9daf125632b06dba71cbc26903
Satyajit <sahoosatyajit2801@gmail.com>
2024-01-23 00:37:56 +0530

    project setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Home = () => {
+    return (
+        <div className="homePageWrapper">
+            <div className="formWrapper">
+                <img src="/code-editor.png" alt="Logo" />
+            </div>
+        </div>
+    )
+}
+
+export default Home
