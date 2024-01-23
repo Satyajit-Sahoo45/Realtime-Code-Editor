c2cfacd96257f98384c267bffd62f1ea0ecf74fa
Satyajit <sahoosatyajit2801@gmail.com>
2024-01-24 01:11:21 +0530

    added home page ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,21 @@ const Home = () => {
     return (
         <div className="homePageWrapper">
             <div className="formWrapper">
-                <img src="/code-editor.png" alt="Logo" />
+                <img className="homePageLogo" src="/logo.svg" alt="Logo" />
+                <h4 className="mainLabel">Paste Invitation ROOM ID</h4>
+                <div className="inputGroup">
+                    <input type="text" className="inputBox" placeholder="ROOM ID" />
+                    <input type="text" className="inputBox" placeholder="USER NAME" />
+                    <button className="btn joinBtn">Join</button>
+                    <span className="createInfo">
+                        If you don't have invite then create &nbsp;
+                        <a href="" className="createNewBtn">New Room</a>
+                    </span>
+                </div>
             </div>
+            <footer>
+                <h4>Built with ❤️ by&nbsp; <a href="https://www.linkedin.com/in/satyajit-sahoo-28052001/"> Satyajit Sahoo </a></h4>
+            </footer>
         </div>
     )
 }
