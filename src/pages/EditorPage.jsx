eff9c5db0d13eeb733961902fb83bde212894ff6
Satyajit <sahoosatyajit2801@gmail.com>
2024-01-28 02:07:59 +0530

    setup websocket and it's functionality

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -1,23 +1,58 @@
-import React, { useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { Client } from "../components/Client";
 import Editor from "../components/Editor";
+import { initSocket } from "../socket";
+import ACTIONS from "../Action";
+import toast from "react-hot-toast";
+import {
+    useLocation,
+    useNavigate,
+    Navigate,
+    useParams,
+} from 'react-router-dom';
 
 const EditorPage = () => {
 
-    const [clients, setClients] = useState([
-        {
-            socketId: 1,
-            username: "Satyajit Sahoo"
-        },
-        {
-            socketId: 2,
-            username: "John Doe"
-        },
-        {
-            socketId: 3,
-            username: "Jane Doe"
+    const socketRef = useRef(null);
+    const location = useLocation();
+    const { roomId } = useParams();
+    const reactNavigator = useNavigate();
+    const [clients, setClients] = useState([]);
+
+
+    useEffect(() => {
+        const init = async () => {
+            socketRef.current = await initSocket()
+            socketRef.current.on('connect_error', (err) => handleErrors(err));
+            socketRef.current.on('connect_failed', (err) => handleErrors(err));
+
+            function handleErrors(e) {
+                console.log('socket error', e);
+                toast.error('Socket connection failed, try again later.');
+                reactNavigator('/');
+            }
+
+            socketRef.current.emit(ACTIONS.JOIN, {
+                roomId,
+                username: location.state?.userName
+            })
+
+            // Listning for joined event
+            socketRef.current.on(ACTIONS.JOINED, ({ clients, username, socketId }) => {
+                if (username !== location.state?.userName) {
+                    toast.success(`${username} joined the room.`)
+                    console.log(`${username} joined!!!`)
+                }
+                setClients(clients)
+            })
         }
-    ]);
+
+        init()
+    }, [])
+
+    if (!location.state) {
+        return <Navigate to="/" />;
+    }
 
     return (
         <div className="mainWrap">
